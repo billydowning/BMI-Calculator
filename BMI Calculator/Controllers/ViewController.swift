1c66118c57171a5603574292731917225a8c8af0
Billy Downing <wldowning@gmail.com>
2021-01-26 15:16:40 -0700

    setup IB variables

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(String(format: "%.0f", sender.value))kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let BMI = (weight / pow(height, 2))
+        print(BMI)
+    }
 }
 
